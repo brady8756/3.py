e88c5bc5d81e09588d706593a27e86621aa4c98c
brady8756 <95857599+brady8756@users.noreply.github.com>
2021-12-24 10:56:59 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -8,17 +8,17 @@ import random
 def drawBoard(board):
     # This function prints out the board that it was passed.
     # "board" is a list of 10 strings representing the board (ignore index 0)
-    st.write('   |   |')
+    st.write('   |      |')
     st.write(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    st.write('   |   |')
+    st.write('   |      |')
     st.write('-----------')
-    st.write('   |   |')
+    st.write('   |      |')
     st.write(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    st.write('   |   |')
+    st.write('   |      |')
     st.write('-----------')
-    st.write('   |   |')
+    st.write('   |      |')
     st.write(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    st.write('   |   |')
+    st.write('   |      |')
 
 def inputPlayerLetter():
     # Lets the player type which letter they want to be.
