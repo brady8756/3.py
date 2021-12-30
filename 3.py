11effa4db3239c4f69495d3977e118a8005bfad5
brady8756 <95857599+brady8756@users.noreply.github.com>
2021-12-30 21:59:25 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -89,8 +89,7 @@ def getPlayerMove(board):
     #while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
     st.write('What is your next move? (1-9)')
     #move = st.sidebar.text_input
-    return int(move)
-    move = move.upper()
+    return move
 
 def chooseRandomMoveFromList(board, movesList):
     # 隨機返回一個可以落子的座標
