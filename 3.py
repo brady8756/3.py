3a0b2eb6397d718486a005a1ba24c29aff774c14
brady8756 <95857599+brady8756@users.noreply.github.com>
2021-12-30 21:33:35 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -20,7 +20,7 @@ def drawBoard(board):
     st.write(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     st.write('   |   |')
 
-letter = st.sidebar.text_input('please input X or O?' , 'X O')
+#letter = st.sidebar.text_input('please input X or O?' , 'X O')
 move = st.sidebar.text_input
 #return = st.sider.text_input.lower().startswith('y')
 
