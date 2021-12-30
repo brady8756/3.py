376a637e17921a76403a50a35432abf51bc7590d
brady8756 <95857599+brady8756@users.noreply.github.com>
2021-12-30 19:50:49 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -24,6 +24,9 @@ def inputPlayerLetter():
     # Lets the player type which letter they want to be.
     # Returns a list with the player’s letter as the first item, and the computer's letter as the second.
     letter = ''
+    
+    st.session
+    
     while not (letter == 'X' or letter == 'O'):
         st.write('Do you want to be X or O?')
         letter = st.text_input('please input X or O?' , 'X O')
