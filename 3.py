198b754e1115552fd875c100858afc355df713d3
brady8756 <95857599+brady8756@users.noreply.github.com>
2021-12-30 21:55:09 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -29,11 +29,11 @@ def inputPlayerLetter():
     # Lets the player type which letter they want to be.
     # Returns a list with the player’s letter as the first item, and the computer's letter as the second.
     letter = ''
-    while not (letter == 'X' or letter == 'O'):
-        st.write('Do you want to be X or O?')
+    #while not (letter == 'X' or letter == 'O'):
+        #st.write('Do you want to be X or O?')
         #letter = st.sider.text_input('please input X or O?' , 'X O')
         #letter = st.sidebar.text_input('please input X or O?' , 'X O')
-        letter = letter.upper()
+    letter = letter.upper()
     
     # the first element in the list is the player’s letter, the second is the computer's letter.
     if letter == 'X':
